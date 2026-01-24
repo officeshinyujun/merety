ee6b39c460b0eefec3f5434de1a7bc174d616370
Yujun shin <officeshinyujun@gmail.com>
2026-01-24 23:01:48 +0900

    feat: Implement and integrate the `StudyListCard` component and an "Add Study" button with new styling on the admin study management page.

--- frontend/src/app/admin/study/page.tsx
+++ frontend/src/app/admin/study/page.tsx
@@ -1,17 +1,37 @@
 import { VStack } from '@/components/general/VStack';
 import s from './style.module.scss';
 import Title from '@/components/study/Title';
+import { HStack } from '@/components/general/HStack';
+import Button from '@/components/general/Button';
+import StudyListCard from '@/components/admin/StudyListCard';
 
-export default function Study(){
+export default function Study() {
     return (
-        <VStack 
-        align='start'
-        justify='start'
-        gap={16}
-        fullHeight
-        fullWidth
-        className={s.container}>
-            <Title text='Study Management'/>
+        <VStack
+            align='start'
+            justify='start'
+            gap={16}
+            fullHeight
+            fullWidth
+            className={s.container}>
+            <Title text='Study Management' />
+            <HStack fullWidth justify='end' align='center'>
+                <Button className={s.addButton}>Add Study</Button>
+            </HStack>
+            <VStack
+                fullWidth
+                fullHeight
+                align='start'
+                justify='start'
+                gap={16}
+                className={s.studyList}
+            >
+                <StudyListCard
+                    name='Study 1'
+                    type='Web'
+                    createdAt='2022-01-01'
+                />
+            </VStack>
         </VStack>
     );
 }
--- frontend/src/components/admin/StudyListCard/index.tsx
+++ frontend/src/components/admin/StudyListCard/index.tsx
@@ -1,15 +1,41 @@
 import { HStack } from "@/components/general/HStack";
 import s from './style.module.scss';
 
-export default function StudyListCard(){
+interface StudyListCardProps {
+    name: string;
+    type: 'Web' | 'Red';
+    createdAt: string;
+}
+
+export default function StudyListCard({
+    name,
+    type,
+    createdAt
+}: StudyListCardProps) {
     return (
         <HStack
-        fullWidth
-        className={s.container}
+            align="center"
+            justify="between"
+            fullWidth
+            className={s.container}
         >
-            <HStack>
-                <p>sss</p>
+            <HStack
+                align="center"
+                justify="start"
+                gap={8}
+                className={s.leftContainer}
+            >
+                <span>{name}</span>
+                <HStack
+                    align="center"
+                    justify="center"
+                    gap={4}
+                    className={s.typeCard}
+                >
+                    <p>{type}</p>
+                </HStack>
             </HStack>
+            <h6>{createdAt}</h6>
         </HStack>
     );
 }
